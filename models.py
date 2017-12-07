93d86676c580ed36246b4019dae461a45a081a3e
wjpark1 <wjpark@berkeley.edu>
2017-12-07 12:43:21 -0800

    Add files via upload

---
+++ models.py
@@ -0,0 +1,445 @@
+import numpy as np
+
+import backend
+import nn
+
+class Model(object):
+    """Base model class for the different applications"""
+    def __init__(self):
+        self.get_data_and_monitor = None
+        self.learning_rate = 0.0
+
+    def run(self, x, y=None):
+        raise NotImplementedError("Model.run must be overriden by subclasses")
+
+    def train(self):
+        """
+        Train the model.
+
+        `get_data_and_monitor` will yield data points one at a time. In between
+        yielding data points, it will also monitor performance, draw graphics,
+        and assist with automated grading. The model (self) is passed as an
+        argument to `get_data_and_monitor`, which allows the monitoring code to
+        evaluate the model on examples from the validation set.
+        """
+        for x, y in self.get_data_and_monitor(self):
+            graph = self.run(x, y)
+            graph.backprop()
+            graph.step(self.learning_rate)
+
+class RegressionModel(Model):
+    """
+    A neural network model for approximating a function that maps from real
+    numbers to real numbers. The network should be sufficiently large to be able
+    to approximate sin(x) on the interval [-2pi, 2pi] to reasonable precision.
+    """
+    def __init__(self):
+        Model.__init__(self)
+        self.get_data_and_monitor = backend.get_data_and_monitor_regression
+
+        # Remember to set self.learning_rate!
+        # You may use any learning rate that works well for your architecture
+        "*** YOUR CODE HERE ***"
+        self.learning_rate = 0.01
+        self.m = nn.Variable(1, 200)
+        self.b = nn.Variable(200)
+        self.m1 = nn.Variable(200,1)
+        self.b1 = nn.Variable(1)
+
+    def run(self, x, y=None):
+        """
+        Runs the model for a batch of examples.
+
+        The correct outputs `y` are known during training, but not at test time.
+        If correct outputs `y` are provided, this method must construct and
+        return a nn.Graph for computing the training loss. If `y` is None, this
+        method must instead return predicted y-values.
+
+        Inputs:
+            x: a (batch_size x 1) numpy array
+            y: a (batch_size x 1) numpy array, or None
+        Output:
+            (if y is not None) A nn.Graph instance, where the last added node is
+                the loss
+            (if y is None) A (batch_size x 1) numpy array of predicted y-values
+
+        Note: DO NOT call backprop() or step() inside this method!
+        """
+        "*** YOUR CODE HERE ***"
+        graph = nn.Graph([self.m, self.b, self.m1, self.b1])
+        input_x = nn.Input(graph, x)
+        xm_1 = nn.MatrixMultiply(graph, input_x, self.m)
+        xm_plus_b_1 = nn.MatrixVectorAdd(graph, xm_1, self.b)
+        hidden_output = nn.ReLU(graph, xm_plus_b_1)
+        xm_2 = nn.MatrixMultiply(graph, hidden_output, self.m1)
+        xm_plus_b_2 = nn.MatrixVectorAdd(graph, xm_2, self.b1)
+        #get output associated with xm_plus_b_2, and return it in the else case below
+        if y is not None:
+            # At training time, the correct output `y` is known.
+            # Here, you should construct a loss node, and return the nn.Graph
+            # that the node belongs to. The loss node must be the last node
+            # added to the graph.
+            "*** YOUR CODE HERE ***"
+            #one more line where you add the node
+            #you should try two layers, no for loop, just do the same calculation using
+            #the previous output as the input for the next layer
+            input_y = nn.Input(graph, y)
+            loss = nn.SquareLoss(graph, xm_plus_b_2, input_y)
+            return graph
+        else:
+            # At test time, the correct output is unknown.
+            # You should instead return your model's prediction as a numpy array
+            "*** YOUR CODE HERE ***"
+            #just return the output, not the graph
+            return graph.get_output(xm_plus_b_2)
+
+class OddRegressionModel(Model):
+    """
+    A neural network model for approximating a function that maps from real
+    numbers to real numbers.
+
+    Unlike RegressionModel, the OddRegressionModel must be structurally
+    constrained to represent an odd function, i.e. it must always satisfy the
+    property f(x) = -f(-x) at all points during training.
+    """
+    def __init__(self):
+        Model.__init__(self)
+        self.get_data_and_monitor = backend.get_data_and_monitor_regression
+
+        # Remember to set self.learning_rate!
+        # You may use any learning rate that works well for your architecture
+        "*** YOUR CODE HERE ***"
+        self.learning_rate = 0.01
+        self.m = nn.Variable(1, 200)
+        self.b = nn.Variable(200)
+        self.m1 = nn.Variable(200,1)
+        self.b1 = nn.Variable(1)
+
+    def run(self, x, y=None):
+        """
+        Runs the model for a batch of examples.
+
+        The correct outputs `y` are known during training, but not at test time.
+        If correct outputs `y` are provided, this method must construct and
+        return a nn.Graph for computing the training loss. If `y` is None, this
+        method must instead return predicted y-values.
+
+        Inputs:
+            x: a (batch_size x 1) numpy array
+            y: a (batch_size x 1) numpy array, or None
+        Output:
+            (if y is not None) A nn.Graph instance, where the last added node is
+                the loss
+            (if y is None) A (batch_size x 1) numpy array of predicted y-values
+
+        Note: DO NOT call backprop() or step() inside this method!
+        """
+        "*** YOUR CODE HERE ***"
+        graph = nn.Graph([self.m, self.b, self.m1, self.b1])
+        
+        input_x = nn.Input(graph, x)
+        xm_1 = nn.MatrixMultiply(graph, input_x, self.m)
+        xm_plus_b_1 = nn.MatrixVectorAdd(graph, xm_1, self.b)
+        hidden_output = nn.ReLU(graph, xm_plus_b_1)
+        xm_2 = nn.MatrixMultiply(graph, hidden_output, self.m1)
+        xm_plus_b_2 = nn.MatrixVectorAdd(graph, xm_2, self.b1)
+        
+        negated_input_x = nn.Input(graph, x*-1)
+        negated_xm_1 = nn.MatrixMultiply(graph, negated_input_x, self.m)
+        negated_xm_plus_b_1 = nn.MatrixVectorAdd(graph, negated_xm_1, self.b)
+        negated_hidden_output = nn.ReLU(graph, negated_xm_plus_b_1)
+        negated_xm_2 = nn.MatrixMultiply(graph, negated_hidden_output, self.m1)
+        negated_xm_plus_b_2 = nn.MatrixVectorAdd(graph, negated_xm_2, self.b1)
+
+        negative_identity = nn.Input(graph, np.array([[-1.0]]))
+        negated_final_output = nn.MatrixMultiply(graph, negated_xm_plus_b_2, negative_identity)
+        odd_function = nn.Add(graph, xm_plus_b_2, negated_final_output)
+
+        if y is not None:
+            # At training time, the correct output `y` is known.
+            # Here, you should construct a loss node, and return the nn.Graph
+            # that the node belongs to. The loss node must be the last node
+            # added to the graph.
+            "*** YOUR CODE HERE ***"
+            input_y = nn.Input(graph, y)
+            #make node corresponding to negative identity matrix
+            #multiply that with negated_xm_plus_b_2 using matrix_multiply function
+            #use the add (NOT add_node) function defined in nn.py
+            #use the result for the second argument to the call to SquareLoss below
+            loss = nn.SquareLoss(graph, odd_function, input_y)
+            return graph
+        else:
+            # At test time, the correct output is unknown.
+            # You should instead return your model's prediction as a numpy array
+            "*** YOUR CODE HERE ***"
+            return graph.get_output(odd_function)
+
+class DigitClassificationModel(Model):
+    """
+    A model for handwritten digit classification using the MNIST dataset.
+
+    Each handwritten digit is a 28x28 pixel grayscale image, which is flattened
+    into a 784-dimensional vector for the purposes of this model. Each entry in
+    the vector is a floating point number between 0 and 1.
+
+    The goal is to sort each digit into one of 10 classes (number 0 through 9).
+
+    (See RegressionModel for more information about the APIs of different
+    methods here. We recommend that you implement the RegressionModel before
+    working on this part of the project.)
+    """
+    def __init__(self):
+        Model.__init__(self)
+        self.get_data_and_monitor = backend.get_data_and_monitor_digit_classification
+
+        # Remember to set self.learning_rate!
+        # You may use any learning rate that works well for your architecture
+        "*** YOUR CODE HERE ***"
+        self.learning_rate = 0.5
+        self.m = nn.Variable(784, 200)
+        self.b = nn.Variable(200)
+        self.m1 = nn.Variable(200, 10)
+        self.b1 = nn.Variable(10)
+
+    def run(self, x, y=None):
+        """
+        Runs the model for a batch of examples.
+
+        The correct labels are known during training, but not at test time.
+        When correct labels are available, `y` is a (batch_size x 10) numpy
+        array. Each row in the array is a one-hot vector encoding the correct
+        class.
+
+        Your model should predict a (batch_size x 10) numpy array of scores,
+        where higher scores correspond to greater probability of the image
+        belonging to a particular class. You should use `nn.SoftmaxLoss` as your
+        training loss.
+
+        Inputs:
+            x: a (batch_size x 784) numpy array
+            y: a (batch_size x 10) numpy array, or None
+        Output:
+            (if y is not None) A nn.Graph instance, where the last added node is
+                the loss
+            (if y is None) A (batch_size x 10) numpy array of scores (aka logits)
+        """
+        "*** YOUR CODE HERE ***"
+        graph = nn.Graph([self.m, self.b, self.m1, self.b1])
+        input_x = nn.Input(graph, x)
+        xm_1 = nn.MatrixMultiply(graph, input_x, self.m)
+        xm_plus_b_1 = nn.MatrixVectorAdd(graph, xm_1, self.b)
+        hidden_output = nn.ReLU(graph, xm_plus_b_1)
+        xm_2 = nn.MatrixMultiply(graph, hidden_output, self.m1)
+        xm_plus_b_2 = nn.MatrixVectorAdd(graph, xm_2, self.b1)
+
+        if y is not None:
+            "*** YOUR CODE HERE ***"
+            input_y = nn.Input(graph, y)
+            loss = nn.SoftmaxLoss(graph, xm_plus_b_2, input_y)
+            return graph
+        else:
+            "*** YOUR CODE HERE ***"
+            return graph.get_output(xm_plus_b_2)
+
+class DeepQModel(Model):
+    """
+    A model that uses a Deep Q-value Network (DQN) to approximate Q(s,a) as part
+    of reinforcement learning.
+
+    (We recommend that you implement the RegressionModel before working on this
+    part of the project.)
+    """
+    def __init__(self):
+        Model.__init__(self)
+        self.get_data_and_monitor = backend.get_data_and_monitor_rl
+
+        self.num_actions = 2
+        self.state_size = 4
+
+        # Remember to set self.learning_rate!
+        # You may use any learning rate that works well for your architecture
+        "*** YOUR CODE HERE ***"
+        self.learning_rate = 0.01
+        self.m = nn.Variable(4, 200)
+        self.b = nn.Variable(200)
+        self.m1 = nn.Variable(200,2)
+        self.b1 = nn.Variable(2)
+
+    def run(self, states, Q_target=None):
+        """
+        Runs the DQN for a batch of states.
+
+        The DQN takes the state and computes Q-values for all possible actions
+        that can be taken. That is, if there are two actions, the network takes
+        as input the state s and computes the vector [Q(s, a_1), Q(s, a_2)]
+
+        When Q_target == None, return the matrix of Q-values currently computed
+        by the network for the input states.
+
+        When Q_target is passed, it will contain the Q-values which the network
+        should be producing for the current states. You must return a nn.Graph
+        which computes the training loss between your current Q-value
+        predictions and these target values, using nn.SquareLoss.
+
+        Inputs:
+            states: a (batch_size x 4) numpy array
+            Q_target: a (batch_size x 2) numpy array, or None
+        Output:
+            (if Q_target is not None) A nn.Graph instance, where the last added
+                node is the loss
+            (if Q_target is None) A (batch_size x 2) numpy array of Q-value
+                scores, for the two actions
+        """
+        "*** YOUR CODE HERE ***"
+        graph = nn.Graph([self.m, self.b, self.m1, self.b1])
+        input_states = nn.Input(graph, states)
+        statesm_1 = nn.MatrixMultiply(graph, input_states, self.m)
+        statesm_plus_b_1 = nn.MatrixVectorAdd(graph, statesm_1, self.b)
+        hidden_output = nn.ReLU(graph, statesm_plus_b_1)
+        statesm_2 = nn.MatrixMultiply(graph, hidden_output, self.m1)
+        statesm_plus_b_2 = nn.MatrixVectorAdd(graph, statesm_2, self.b1)
+
+        if Q_target is not None:
+            "*** YOUR CODE HERE ***"
+            input_q_target = nn.Input(graph, Q_target)
+            loss = nn.SquareLoss(graph, statesm_plus_b_2, input_q_target)
+            return graph
+        else:
+            "*** YOUR CODE HERE ***"
+            return graph.get_output(statesm_plus_b_2)
+
+    def get_action(self, state, eps):
+        """
+        Select an action for a single state using epsilon-greedy.
+
+        Inputs:
+            state: a (1 x 4) numpy array
+            eps: a float, epsilon to use in epsilon greedy
+        Output:
+            the index of the action to take (either 0 or 1, for 2 actions)
+        """
+        if np.random.rand() < eps:
+            return np.random.choice(self.num_actions)
+        else:
+            scores = self.run(state)
+            return int(np.argmax(scores))
+
+
+class LanguageIDModel(Model):
+    """
+    A model for language identification at a single-word granularity.
+
+    (See RegressionModel for more information about the APIs of different
+    methods here. We recommend that you implement the RegressionModel before
+    working on this part of the project.)
+    """
+    def __init__(self):
+        Model.__init__(self)
+        self.get_data_and_monitor = backend.get_data_and_monitor_lang_id
+
+        # Our dataset contains words from five different languages, and the
+        # combined alphabets of the five languages contain a total of 47 unique
+        # characters.
+        # You can refer to self.num_chars or len(self.languages) in your code
+        self.num_chars = 47
+        self.languages = ["English", "Spanish", "Finnish", "Dutch", "Polish"]
+
+        # Remember to set self.learning_rate!
+        # You may use any learning rate that works well for your architecture
+        self.d = 200
+        self.learning_rate = 0.01
+        self.b1 = nn.Variable(400)
+        self.b2 = nn.Variable(self.d)
+        self.b3 = nn.Variable(5)
+        self.W1 = nn.Variable(self.d, 400)
+        self.W2 = nn.Variable(400, self.d)
+        self.W3 = nn.Variable(self.d, 5)
+        
+        #ed
+        
+        self.h0 = nn.Variable(self.d)
+        self.c = nn.Variable(self.num_chars, self.d)
+
+    def run(self, xs, y=None):
+        """
+        Runs the model for a batch of examples.
+
+        batxh xs[0].shape[0
+        c's individual elements inside array]
+        result = batchxd multiply dx5 add c
+
+        Although words have different lengths, our data processing guarantees
+        that within a single batch, all words will be of the same length (L).
+
+        Here xs will be a list of length L. Each element of xs will be a
+        (batch_size x self.num_chars) numpy array, where every row in the array
+        is a one-hot vector encoding of a character. For example, if we have a
+        batch of 8 three-letter words where the last word is "cat", we will have
+        xs[1][7,0] == 1. Here the index 0 reflects the fact that the letter "a"
+        is the inital (0th) letter of our combined alphabet for this task.
+
+        The correct labels are known during training, but not at test time.
+        When correct labels are available, y is a (batch_size x 5) numpy
+        array. Each row in the array is a one-hot vector encoding the correct
+        class.
+
+        Your model should use a Recurrent Neural Network to summarize the list
+        xs into a single node that represents a (batch_size x hidden_size)
+        array, for your choice of hidden_size. It should then calculate a
+        (batch_size x 5) numpy array of scores, where higher scores correspond
+        to greater probability of the word originating from a particular
+        language. You should use nn.SoftmaxLoss as your training loss.
+
+        Inputs:
+            xs: a list with L elements (one per character), where each element
+                is a (batch_size x self.num_chars) numpy array
+            y: a (batch_size x 5) numpy array, or None
+        Output:
+            (if y is not None) A nn.Graph instance, where the last added node is
+                the loss
+            (if y is None) A (batch_size x 5) numpy array of scores (aka logits)
+
+        Hint: you may use the batch_size variable in your code
+        """
+        batch_size = xs[0].shape[0]
+        graph = nn.Graph([self.b1, self.b2, self.b3, self.W1, self.W2, self.W3, self.h0, self.c])
+        
+        #ed 
+        H0 = nn.Input(graph, np.zeros((batch_size, self.d)))
+        H = nn.MatrixVectorAdd(graph, H0, self.h0)
+        # now you have H initialized
+        
+        for val in xs:
+            input_c = nn.Input(graph, val)
+            c = nn.MatrixMultiply(graph, input_c, self.c)
+            h_update = nn.MatrixVectorAdd(graph, H, c)
+
+            # NOW YOU HAVE A NODE WITH A MATRIX (batch_size x d)
+            # PASTE YOUR USUAL ARCHITECURE AND MAKE SURE IT HAS AT LEAST ONE RELU NODE
+            # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            # MAKE SURE H AT THE END OF EACH LOOP IS (batch_size x d)
+            # MAKE SURE H AT THE END OF EACH LOOP IS (batch_size x d)
+            # MAKE SURE H AT THE END OF EACH LOOP IS (batch_size x d)
+            # MAKE SURE H AT THE END OF EACH LOOP IS (batch_size x d)
+            # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+            xm_1 = nn.MatrixMultiply(graph, h_update, self.W1)
+            xm_plus_b_1 = nn.MatrixVectorAdd(graph, xm_1, self.b1)
+            hidden_output = nn.ReLU(graph, xm_plus_b_1)
+            xm_2 = nn.MatrixMultiply(graph, hidden_output, self.W2)
+            H = nn.MatrixVectorAdd(graph, xm_2, self.b2)
+            
+        # OK LAST STEP
+        # DO SOME MATRIX TRANSFORMATIONS SO THAT H is (batch_size x 5)
+        # USE THE VARIABLES YOU INITIALIZED IN PREVIOUS QUESTIONS
+        # e.g.
+        xm = nn.MatrixMultiply(graph, H, self.W3)
+        x_m_2_plus_b = nn.MatrixVectorAdd(graph, xm, self.b3)
+        "* YOUR CODE HERE *"
+        
+        if y is not None:
+            input_y = nn.Input(graph, y)
+            loss = nn.SoftmaxLoss(graph, x_m_2_plus_b, input_y)
+            return graph
+        else:
+            return graph.get_output(x_m_2_plus_b)
